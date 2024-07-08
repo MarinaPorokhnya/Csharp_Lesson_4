0fd472d0347ba1c676f098bc5de9fd0c058fd9fc
MarinaPorokhnya <marina.vbyaby.poroxnya@mail.ru>
2024-07-08 18:44:45 +0300

    Initial commit

---
+++ Task_01/Program.cs
@@ -0,0 +1,75 @@
+﻿// Двумерные массивы
+// Массив массивов - матрица.
+
+// int[,] table = new int[3, 5]; //[,] номер строки, номер элемента
+//                               // 3 - количество строк, 5 - количество элементов в строке
+//                               // table - имя
+
+// Способы создания двумерного массива
+// int[,] table = new int[2, 4] {
+//     { 1, 1, 1, 1 },
+//     { 2, 2, 2, 2 }
+// };
+
+// int[,] table2 = new int[,] {
+//     { 1, 1, 1, 1 },
+//     { 2, 2, 2, 2 }
+// };
+
+// int[,] table3 = {
+//     { 1, 1, 1, 1 },
+//     { 2, 2, 2, 2 }
+// };
+
+// Задача 1
+// Создать двумерный массив с размерами
+// 3х5, состоящий из целых чисел.
+// Вывести его элементы на экран.
+// int[,] matrix = new int[3, 5];
+
+// Random rnd = new Random();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+
+//         matrix[i, j] = rnd.Next(1, 11);
+//     }
+// }
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]} ");// Интерполяция строк.
+//     }
+//     Console.WriteLine();
+// }
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
---
+++ Task_02/Program.cs
@@ -0,0 +1,40 @@
+﻿// 1- Функция выделяет память под массив некоторого размера 
+//   и заполняет его случайными целыми числами.
+// 
+// 2- Для выделения участка памяти,нужно знать его размер.Размер матрицы в виде 
+//    числа строк: rowsCount и числа столбцов: ColumnsCount,
+//    передаем в качестве параметра. 
+// 
+// 3- Двумерный массив функция возвращает в качестве возвращаемого значения: int [,].
+// 
+// Функция CreateMatrix(Двумерный массив),которая генерирует двумерный массив
+// из случайных целых чисел:
+
+int [,] CreatMatrix(int rowsCount, int ColumnsCount)
+{
+    int [,] matrix = new int [rowsCount, ColumnsCount];
+
+    Random rnd = new Random ();
+    for (int i = 0; i < matrix.GetLength (0); i ++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix; // Возврат значения.
+}
+void ShowMatrix(int [,] matrix) //Функция для вывода массива на экран.Не возвращает значение.
+{
+    for(int i = 0; i < matrix.GetLength(0); i ++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j ++)
+        {
+
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = CreatMatrix(4, 5);
+ShowMatrix(matrix);
